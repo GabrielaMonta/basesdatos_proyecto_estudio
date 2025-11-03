56c67d928173c3a133144fb7be0c994b19de287e
GabrielaMonta <gabrielaam201@gmail.com>
2025-11-03 13:04:30 -0300

    SQL corregido

--- script/script_ddl_proyecto.sql
+++ script/script_ddl_proyecto.sql
@@ -6,7 +6,7 @@ GO
 
 CREATE TABLE cargo_eleccion
 (
-  cargo_id INT NOT NULL,
+  cargo_id INT IDENTITY(1,1) NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   descripcion VARCHAR(200) NOT NULL,
   CONSTRAINT PK_cargo_eleccion PRIMARY KEY (cargo_id)
@@ -24,17 +24,16 @@ CREATE TABLE estudiante
 );
 
 
-
 CREATE TABLE carrera
 (
-  carrera_id INT NOT NULL,
+  carrera_id INT IDENTITY(1,1) NOT NULL,
   nombre_carrera VARCHAR(100) NOT NULL,
   CONSTRAINT PK_carrera PRIMARY KEY (carrera_id)
 );
 
 CREATE TABLE universidad
 (
-  universidad_id INT NOT NULL,
+  universidad_id INT IDENTITY(1,1) NOT NULL,
   nombre VARCHAR(200) NOT NULL,
   CONSTRAINT PK_universidad PRIMARY KEY (universidad_id)
 );
@@ -51,17 +50,17 @@ CREATE TABLE facultad
 
 CREATE TABLE estado_carrera
 (
-  estado_id INT NOT NULL,
+  estado_id INT IDENTITY(1,1)NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   CONSTRAINT PK_estado_carrera PRIMARY KEY (estado_id)
 );
 
 CREATE TABLE carrera_estudiante
 (
-  año_ingreso DATE NOT NULL,
-  año_egreso DATE,
   carrera_id INT NOT NULL,
   documento_estudiante INT NOT NULL,
+  año_ingreso DATE NOT NULL,
+  año_egreso DATE,
   estado_id INT NOT NULL,
   CONSTRAINT PK_carrera_estudiante PRIMARY KEY (carrera_id, documento_estudiante),
   CONSTRAINT FK_carrera_estudiante_carrera FOREIGN KEY (carrera_id) REFERENCES carrera(carrera_id),
@@ -75,20 +74,20 @@ CREATE TABLE facultad_carrera
   universidad_id INT NOT NULL,
   carrera_id INT NOT NULL,
   CONSTRAINT PK_facultad_carrera PRIMARY KEY (facultad_id, universidad_id, carrera_id),
-  CONSTRAINT FK_facultad_carrera_facultad FOREIGN KEY (facultad_id, universidad_id) REFERENCES facultad(facultad_id ),
+  CONSTRAINT FK_facultad_carrera_facultad FOREIGN KEY (facultad_id, universidad_id) REFERENCES facultad(universidad_id, facultad_id),
   CONSTRAINT FK_facultad_carrera_carrera FOREIGN KEY (carrera_id) REFERENCES carrera(carrera_id)
 );
 
 CREATE TABLE banca
 (
-  banca_id INT NOT NULL,
+  banca_id INT IDENTITY(1,1) NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   CONSTRAINT PK_banca PRIMARY KEY (banca_id)
 );
 
 CREATE TABLE orden
 (
-  orden_id INT NOT NULL,
+  orden_id INT IDENTITY(1,1) NOT NULL,
   numero INT NOT NULL,
   CONSTRAINT PK_orden PRIMARY KEY (orden_id)
 );
@@ -100,20 +99,20 @@ CREATE TABLE cargo_banca
   orden_id INT NOT NULL,
   CONSTRAINT PK_cargo_banca PRIMARY KEY (cargo_id, banca_id, orden_id),
   CONSTRAINT FK_cargo_banca_cargo_eleccion FOREIGN KEY (cargo_id) REFERENCES cargo_eleccion(cargo_id),
-  FOREIGN KEY FK_cargo_banca_banca (banca_id) REFERENCES banca(banca_id),
-  FOREIGN KEY FK_cargo_banca_orden (orden_id) REFERENCES orden(orden_id)
+  CONSTRAINT FK_cargo_banca_banca FOREIGN KEY (banca_id) REFERENCES banca(banca_id),
+  CONSTRAINT FK_cargo_banca_orden  FOREIGN KEY (orden_id) REFERENCES orden(orden_id)
 );
 
 CREATE TABLE partido
 (
-  partido_id INT NOT NULL,
+  partido_id INT IDENTITY(1,1) NOT NULL,
   nombre_partido VARCHAR(100) NOT NULL,
   CONSTRAINT PK_partido PRIMARY KEY (partido_id)
 );
 
 CREATE TABLE lista
 (
-  lista_id INT NOT NULL,
+  lista_id INT IDENTITY(1,1) NOT NULL,
   partido_id INT NOT NULL,
   CONSTRAINT PK_lista PRIMARY KEY (lista_id),
   CONSTRAINT FK_lista_partido FOREIGN KEY (partido_id) REFERENCES partido(partido_id)
@@ -126,8 +125,8 @@ CREATE TABLE cargo_banca_estudiante
   orden_id INT NOT NULL,
   documento_estudiante INT NOT NULL,
   CONSTRAINT PK_cargo_banca_estudiante PRIMARY KEY (cargo_id, banca_id, orden_id, documento_estudiante),
-  CONSTRAINT FK_cargo_banca_estudiante_cargo_banca FOREIGN KEY (cargo_id, banca_id, orden_id) REFERENCES cargo_banca(, , ),
- CONSTRAINT FK_cargo_banca_estudiante_estudiante FOREIGN KEY (documento_estudiante) REFERENCES estudiante(documento_estudiante)
+  CONSTRAINT FK_cargo_banca_estudiante_cargo_banca FOREIGN KEY (cargo_id, banca_id, orden_id) REFERENCES cargo_banca(cargo_id, banca_id, orden_id),
+  CONSTRAINT FK_cargo_banca_estudiante_estudiante FOREIGN KEY (documento_estudiante) REFERENCES estudiante(documento_estudiante)
 );
 
 CREATE TABLE lista_cargos
@@ -137,24 +136,22 @@ CREATE TABLE lista_cargos
   banca_id INT NOT NULL,
   orden_id INT NOT NULL,
   documento_estudiante INT NOT NULL,
-  CONSTRAINT PK lista_cargos PRIMARY KEY (lista_id, cargo_id, banca_id, orden_id, documento_estudiante),
+  CONSTRAINT PK_lista_cargos PRIMARY KEY (lista_id, cargo_id, banca_id, orden_id, documento_estudiante),
   CONSTRAINT FK_lista_cargos_lista FOREIGN KEY (lista_id) REFERENCES lista(lista_id),
-  CONSTRAINT FK_lista_cargos_cargo_banca_estudiante FOREIGN KEY (cargo_id, banca_id, orden_id, documento_estudiante) REFERENCES cargo_banca_estudiante(, , , )
+  CONSTRAINT FK_lista_cargos_cargo_banca_estudiante FOREIGN KEY (cargo_id, banca_id, orden_id, documento_estudiante) REFERENCES cargo_banca_estudiante(cargo_id, banca_id, orden_id, documento_estudiante)
 );
-
-
 
 
 CREATE TABLE eleccion
 (
-  eleccion_id INT NOT NULL,
-  año DATE NOT NULL,
+  eleccion_id INT IDENTITY(1,1) NOT NULL,
+  año INT NOT NULL DEFAULT YEAR(GETDATE()),
   CONSTRAINT PK_eleccion PRIMARY KEY (eleccion_id)
 );
 
 CREATE TABLE mesa_identificacion
 (
-  mesa_identificacion_id INT NOT NULL,
+  mesa_identificacion_id INT IDENTITY(1,1) NOT NULL,
   nro_mesa_identificacion INT NOT NULL,
   documento_estudiante INT NOT NULL,
   eleccion_id INT NOT NULL,
@@ -165,30 +162,30 @@ CREATE TABLE mesa_identificacion
 
 CREATE TABLE token_votante
 (
-  token_id INT NOT NULL,
-  codigo_token varbinary NOT NULL,
-  usado CHAR(2) NOT NULL,
-  mesa_identificacion_is INT NOT NULL,
+  token_id INT IDENTITY(1,1) NOT NULL,
+  codigo_token varbinary(64) NOT NULL,
+  usado BIT NOT NULL DEFAULT 0,
+  mesa_identificacion_id INT NOT NULL,
   CONSTRAINT PK_token_votante PRIMARY KEY (token_id),
-  CONSTRAINT FK_token_votante_mesa_identificacion FOREIGN KEY (mesa_identificacion_is) REFERENCES mesa_identificacion(mesa_identificacion_id),
-CONSTRAINT UQ_codigo_token UNIQUE (codigo_token)
+  CONSTRAINT FK_token_votante_mesa_identificacion FOREIGN KEY (mesa_identificacion_id) REFERENCES mesa_identificacion(mesa_identificacion_id),
+  CONSTRAINT UQ_codigo_token UNIQUE (codigo_token)
 );
 
 
 CREATE TABLE mesa_votacion
 (
-  mesa_votacion_id INT NOT NULL,
+  mesa_votacion_id INT IDENTITY(1,1) NOT NULL,
   nro_mesa_votacion INT NOT NULL,
   CONSTRAINT PK_mesa_votacion PRIMARY KEY (mesa_votacion_id)
 );
 
 CREATE TABLE voto
 (
-  fecha_emision DATE NOT NULL,
-  lista_id INT NOT NULL,
   token_id INT NOT NULL,
+  fecha_emision DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+  lista_id INT NOT NULL,
   mesa_votacion_id INT NOT NULL,
- CONSTRAINT PK_voto  PRIMARY KEY (lista_id),
+  CONSTRAINT PK_voto  PRIMARY KEY (token_id),
   CONSTRAINT FK_voto_lista FOREIGN KEY (lista_id) REFERENCES lista(lista_id),
   CONSTRAINT FK_voto_token_votante FOREIGN KEY (token_id) REFERENCES token_votante(token_id),
   CONSTRAINT FK_voto_mesa_votacion FOREIGN KEY (mesa_votacion_id) REFERENCES mesa_votacion(mesa_votacion_id)
@@ -196,9 +193,9 @@ CREATE TABLE voto
 
 CREATE TABLE resultado_eleccion
 (
-  resultado INT NOT NULL,
   lista_id INT NOT NULL,
   eleccion_id INT NOT NULL,
+  resultado INT NOT NULL,
   CONSTRAINT PK_resultado_eleccion PRIMARY KEY (lista_id, eleccion_id),
   CONSTRAINT FK_resultado_eleccion_lista FOREIGN KEY (lista_id) REFERENCES lista(lista_id),
   CONSTRAINT FK_resultado_eleccion_eleccion FOREIGN KEY (eleccion_id) REFERENCES eleccion(eleccion_id)
@@ -206,9 +203,9 @@ CREATE TABLE resultado_eleccion
 
 CREATE TABLE escrutinio_mesa
 (
-  cantidad_votos INT NOT NULL,
   lista_id INT NOT NULL,
   mesa_votacion_id INT NOT NULL,
+  cantidad_votos INT NOT NULL,
   CONSTRAINT PK_escrutinio_mesa PRIMARY KEY (lista_id, mesa_votacion_id),
   CONSTRAINT FK_escrutinio_mesa_lista FOREIGN KEY (lista_id) REFERENCES lista(lista_id),
   CONSTRAINT FK_escrutinio_mesa_mesa_votacion FOREIGN KEY (mesa_votacion_id) REFERENCES mesa_votacion(mesa_votacion_id)
